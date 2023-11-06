7fc7f6b88c21d78ecfe8c8506121f3f641e1c401
NathanKell <nathankell@gmail.com>
2023-11-05 23:59:56 -0800

    Port most RP-1 utilities

---
+++ Source/ROUtils/Utils/MiscUtils.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace ROUtils
+{
+    public static class MiscUtils
+    {
+        public static TValue ValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key)
+        {
+            dict.TryGetValue(key, out TValue value);
+            return value;
+        }
+
+        public static T Pop<T>(this List<T> list)
+        {
+            T val = list[0];
+            list.RemoveAt(0);
+            return val;
+        }
+
+        public static Texture2D GetReadableCopy(this Texture2D texture)
+        {
+            Texture2D readable = new Texture2D(texture.width, texture.height);
+
+            RenderTexture tmp = RenderTexture.GetTemporary(
+                    texture.width,
+                    texture.height,
+                    0,
+                    RenderTextureFormat.Default,
+                    RenderTextureReadWrite.Linear);
+
+            Graphics.Blit(texture, tmp);
+            RenderTexture previous = RenderTexture.active;
+            RenderTexture.active = tmp;
+            
+            readable.ReadPixels(new Rect(0, 0, tmp.width, tmp.height), 0, 0);
+            readable.Apply();
+
+            RenderTexture.active = previous;
+            RenderTexture.ReleaseTemporary(tmp);
+
+            return readable;
+        }
+
+        public static Sprite ReplaceSprite(Sprite sprite, string path, SpriteMeshType type)
+        {
+            return Sprite.Create(GameDatabase.Instance.GetTexture(path, false),
+                sprite.rect,
+                sprite.pivot,
+                sprite.pixelsPerUnit,
+                0,
+                type,
+                sprite.border);
+        }
+
+        public static string ToCommaString<T>(this List<T> list)
+        {
+            if (list.Count == 0)
+                return string.Empty;
+
+            var sb = StringBuilderCache.Acquire();
+            sb.Append(list[0].ToString());
+            for (int i = 1, iC = list.Count; i < iC; ++i)
+                sb.Append(", " + list[i].ToString());
+
+            return sb.ToStringAndRelease();
+        }
+
+        /// <summary>
+        /// NOTE: Must be used only on value-type lists
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        /// <param name="str"></param>
+        public static void FromCommaString<T>(this List<T> list, string str)
+        {
+            Type type = typeof(T);
+            KSPCommunityFixes.Modding.DataType dataType = KSPCommunityFixes.Modding.FieldData.ValueDataType(type);
+
+            list.Clear();
+            var split = str.Split(',');
+            foreach (var s in split)
+            {
+                string s2 = s.Trim();
+                if (s2.Length == 0)
+                    continue;
+                list.Add((T)KSPCommunityFixes.Modding.FieldData.ReadValue(s2, dataType, type));
+            }
+        }
+
+        public static bool AllTrue(this List<bool> list)
+        {
+            for (int i = list.Count; i-- > 0;)
+                if (!list[i])
+                    return false;
+
+            return true;
+        }
+
+        public static bool AllFalse(this List<bool> list)
+        {
+            for (int i = list.Count; i-- > 0;)
+                if (list[i])
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ Source/ROUtils/Utils/ModUtils.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Reflection;
+using System.Linq;
+
+// ReSharper disable once CheckNamespace
+namespace ROUtils
+{
+    public static class ModUtils
+    {
+        private static bool? _isTestFlightInstalled = null;
+        private static bool? _isTestLiteInstalled = null;
+
+        private static PropertyInfo _piTFInstance;
+        private static PropertyInfo _piTFSettingsEnabled;
+        private static Type _tlSettingsType;
+        private static FieldInfo _fiTLSettingsDisabled;
+
+        public static bool IsTestFlightInstalled
+        {
+            get
+            {
+                if (!_isTestFlightInstalled.HasValue)
+                {
+                    Assembly a = AssemblyLoader.loadedAssemblies.FirstOrDefault(la => string.Equals(la.name, "TestFlightCore", StringComparison.OrdinalIgnoreCase))?.assembly;
+                    _isTestFlightInstalled = a != null;
+                    if (_isTestFlightInstalled.Value)
+                    {
+                        Type t = a.GetType("TestFlightCore.TestFlightManagerScenario");
+                        _piTFInstance = t?.GetProperty("Instance", BindingFlags.Public | BindingFlags.Static);
+                        _piTFSettingsEnabled = t?.GetProperty("SettingsEnabled", BindingFlags.Public | BindingFlags.Instance | BindingFlags.FlattenHierarchy);
+                    }
+                }
+                return _isTestFlightInstalled.Value;
+            }
+        }
+
+        public static bool IsTestLiteInstalled
+        {
+            get
+            {
+                if (!_isTestLiteInstalled.HasValue)
+                {
+                    Assembly a = AssemblyLoader.loadedAssemblies.FirstOrDefault(la => string.Equals(la.name, "TestLite", StringComparison.OrdinalIgnoreCase))?.assembly;
+                    _isTestLiteInstalled = a != null;
+                    if (_isTestLiteInstalled.Value)
+                    {
+                        _tlSettingsType = a.GetType("TestLite.TestLiteGameSettings");
+                        _fiTLSettingsDisabled = _tlSettingsType?.GetField("disabled");
+                    }
+                }
+                return _isTestLiteInstalled.Value;
+            }
+        }
+
+        public static void ToggleFailures(bool isEnabled)
+        {
+            if (IsTestFlightInstalled) ToggleTFFailures(isEnabled);
+            else if (IsTestLiteInstalled) ToggleTLFailures(isEnabled);
+        }
+
+        public static void ToggleTFFailures(bool isEnabled)
+        {
+            object tfInstance = _piTFInstance.GetValue(null);
+            _piTFSettingsEnabled.SetValue(tfInstance, isEnabled);
+        }
+
+        private static void ToggleTLFailures(bool isEnabled)
+        {
+            _fiTLSettingsDisabled.SetValue(HighLogic.CurrentGame.Parameters.CustomParams(_tlSettingsType), !isEnabled);
+            GameEvents.OnGameSettingsApplied.Fire();
+        }
+    }
+}
