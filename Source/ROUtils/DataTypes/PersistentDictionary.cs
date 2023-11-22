0a1093a2eddabdb3fa2fe7e0a2bdf46d77cf11f8
NathanKell <nathankell@gmail.com>
2023-11-22 02:30:09 -0800

    More refactoring of Dictionary to reduce repeated code. Now save/load is handled by a separate class so Dictionary and SortedList can use the same code. Also improve NodeKeyed so it supports any value-type (as NodeValueKeyed). Move collection helpers (and these new save/load classes) to DictionaryHelpers.cs. Should be able to do the same refactor to list and hashset to make CollectionHelpers

--- Source/ROUtils/DataTypes/PersistentDictionary.cs
+++ Source/ROUtils/DataTypes/PersistentDictionary.cs
@@ -5,165 +5,48 @@ using UnityEngine;
 
 namespace ROUtils.DataTypes
 {
-    public abstract class PersistentDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IConfigNode where TValue : IConfigNode
+    public abstract class PersistentDictionary<TKey, TValue> : Dictionary<TKey, TValue>, IConfigNode
     {
-        protected static readonly Type _ValueType = typeof(TValue);
-        protected static readonly string _ValueTypeName = typeof(TValue).Name;
-        protected static readonly Dictionary<string, Type> _TypeCache = new Dictionary<string, Type>();
-
-        protected int version; // will be set on load but not used on save
-
-        protected abstract TKey GetKey(int i, ConfigNode keyNode);
-        protected abstract void AddKey(TKey key, ConfigNode keyNode);
-
-        protected TValue GetValue(int i, ConfigNode valueNode)
-        {
-            var n = valueNode.nodes[i];
-            TValue value;
-            if (version == 1 || n.name == "VALUE" || n.name == _ValueTypeName)
-            {
-                value = Activator.CreateInstance<TValue>();
-            }
-            else
-            {
-                if (!_TypeCache.TryGetValue(n.name, out var type))
-                    type = HarmonyLib.AccessTools.TypeByName(n.name);
-                if (type == null || !_ValueType.IsAssignableFrom(type))
-                    type = _ValueType;
-                else
-                    _TypeCache[n.name] = type;
-
-                value = (TValue)Activator.CreateInstance(type);
-            }
-            value.Load(n);
-            return value;
-        }
-
-        protected void AddValue(TValue value, ConfigNode valueNode)
-        {
-            var type = value.GetType();
-            ConfigNode n = new ConfigNode(type == _ValueType ? _ValueTypeName : type.FullName);
-            value.Save(n);
-            valueNode.AddNode(n);
-        }
-
         public void Load(ConfigNode node)
         {
-            Clear();
-            ConfigNode keyNode = node.nodes[0];
-            ConfigNode valueNode = node.nodes[1];
-            version = 1;
-            node.TryGetValue("version", ref version);
-
-            for (int i = 0; i < valueNode.nodes.Count; ++i)
-            {
-                TKey key = GetKey(i, keyNode);
-                TValue value = GetValue(i, valueNode);
-                Add(key, value);
-            }
+            ConfigNode.LoadObjectFromConfig(this, node);
         }
 
         public void Save(ConfigNode node)
         {
-            node.AddValue("version", 2);
-            ConfigNode keyNode = node.AddNode("Keys");
-            ConfigNode valueNode = node.AddNode("Values");
-
-            foreach (var kvp in this)
-            {
-                AddKey(kvp.Key, keyNode);
-                AddValue(kvp.Value, valueNode);
-            }
+            ConfigNode.CreateConfigFromObject(this, node);
         }
     }
 
     public class PersistentDictionaryBothObjects<TKey, TValue> : PersistentDictionary<TKey, TValue>, IConfigNode where TKey : IConfigNode where TValue : IConfigNode
     {
-        protected static readonly Type _KeyType = typeof(TKey);
-        protected static readonly string _KeyTypeName = typeof(TKey).Name;
+        [Persistent]
+        protected IDictionaryPersistenceBothObjects<TKey, TValue> _handler;
 
-        protected override TKey GetKey(int i, ConfigNode keyNode)
-        {
-            var n = keyNode.nodes[i];
-            TKey key;
-            if (version == 1 || n.name == "KEY" || n.name == _KeyTypeName)
-            {
-                key = Activator.CreateInstance<TKey>();
-            }
-            else
-            {
-                if (!_TypeCache.TryGetValue(n.name, out var type))
-                    type = HarmonyLib.AccessTools.TypeByName(n.name);
-                if (type == null || !_KeyType.IsAssignableFrom(type))
-                    type = _KeyType;
-                else
-                    _TypeCache[n.name] = type;
-
-                key = (TKey)Activator.CreateInstance(type);
-            }
-            key.Load(n);
-            return key;
-        }
-
-        protected override void AddKey(TKey key, ConfigNode keyNode)
-        {
-            var type = key.GetType();
-            ConfigNode n = new ConfigNode(type == _KeyType ? _KeyTypeName : type.FullName);
-            key.Save(n);
-            keyNode.AddNode(n);
-        }
+        public PersistentDictionaryBothObjects() { _handler = new IDictionaryPersistenceBothObjects<TKey, TValue>(this); }
     }
 
-    public class PersistentDictionaryNodeKeyed<TValue> : PersistentDictionary<string, TValue>, IConfigNode where TValue : IConfigNode
+    /// <summary>
+    /// This does not have a struct constraint because string is not a valuetype but can be handled by ConfigNode's parser
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class PersistentDictionaryNodeValueKeyed<TKey, TValue> : PersistentDictionary<TKey, TValue>, IConfigNode where TValue : IConfigNode
     {
-        private string _keyName = "name";
+        [Persistent]
+        protected IDictionaryPersistenceNodeValueKeyed<TKey, TValue> _handler;
 
-        public PersistentDictionaryNodeKeyed() {}
+        public PersistentDictionaryNodeValueKeyed() { _handler = new IDictionaryPersistenceNodeValueKeyed<TKey, TValue>(this); }
         
-        public PersistentDictionaryNodeKeyed(string keyName)
-        {
-            _keyName = keyName;
-        }
+        public PersistentDictionaryNodeValueKeyed(string keyName) { _handler = new IDictionaryPersistenceNodeValueKeyed<TKey, TValue>(this, keyName); }
+    }
 
-        protected override string GetKey(int i, ConfigNode keyNode)
-        {
-            return keyNode.nodes[i].GetValue(_keyName);
-        }
-
-        protected override void AddKey(string key, ConfigNode keyNode)
-        {
-            keyNode.SetValue(_keyName, key, true);
-        }
-
-        public new void Load(ConfigNode node)
-        {
-            Clear();
-            version = 1;
-            node.TryGetValue("version", ref version);
-            for (int i = 0; i < node.nodes.Count; ++i)
-            {
-                string key = GetKey(i, node);
-                if (string.IsNullOrEmpty(key))
-                {
-                    Debug.LogError("PersistentDictionaryNodeKeyed: null or empty key in node! Skipping. Node=\n" + node.nodes[i].ToString());
-                    continue;
-                }
-
-                TValue value = GetValue(i, node);
-                Add(key, value);
-            }
-        }
-
-        public new void Save(ConfigNode node)
-        {
-            node.AddValue("version", 2);
-            foreach (var kvp in this)
-            {
-                AddValue(kvp.Value, node); // this creates the node
-                // and it will be the last node. So put the key there.
-                node.nodes[node.nodes.Count - 1].SetValue(_keyName, kvp.Key, true);
-            }
-        }
+    /// <summary>
+    /// String-only version of this, for back-compat
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    public class PersistentDictionaryNodeKeyed<TValue> : PersistentDictionaryNodeValueKeyed<string, TValue>, IConfigNode where TValue : IConfigNode
+    {
     }
 
     /// <summary>
@@ -173,18 +56,10 @@ namespace ROUtils.DataTypes
     /// <typeparam name="TValue"></typeparam>
     public class PersistentDictionaryValueTypeKey<TKey, TValue> : PersistentDictionary<TKey, TValue> where TValue : IConfigNode
     {
-        private static readonly Type _KeyType = typeof(TKey);
-        private static readonly DataType _KeyDataType = FieldData.ValueDataType(_KeyType);
+        [Persistent]
+        protected IDictionaryPersistenceValueTypeKey<TKey, TValue> _handler;
 
-        protected override TKey GetKey(int i, ConfigNode keyNode)
-        {
-            return (TKey)FieldData.ReadValue(keyNode.values[i].value, _KeyDataType, _KeyType);
-        }
-
-        protected override void AddKey(TKey key, ConfigNode keyNode)
-        {
-            keyNode.AddValue("key", FieldData.WriteValue(key, _KeyDataType));
-        }
+        public PersistentDictionaryValueTypeKey() { _handler = new IDictionaryPersistenceValueTypeKey<TKey, TValue>(this); }
     }
 
     /// <summary>
@@ -193,39 +68,12 @@ namespace ROUtils.DataTypes
     /// </summary>
     /// <typeparam name="TKey"></typeparam>
     /// <typeparam name="TValue"></typeparam>
-    public class PersistentDictionaryValueTypes<TKey, TValue> : Dictionary<TKey, TValue>, ICloneable, IConfigNode
+    public class PersistentDictionaryValueTypes<TKey, TValue> : PersistentDictionary<TKey, TValue>, ICloneable, IConfigNode
     {
-        private static Type _KeyType = typeof(TKey);
-        private static readonly DataType _KeyDataType = FieldData.ValueDataType(_KeyType);
-        private static Type _ValueType = typeof(TValue);
-        private static readonly DataType _ValueDataType = FieldData.ValueDataType(_ValueType);
+        [Persistent]
+        protected IDictionaryPersistenceValueTypes<TKey, TValue> _handler;
 
-        public void Load(ConfigNode node)
-        {
-            Clear();
-            foreach (ConfigNode.Value v in node.values)
-            {
-                TKey key = (TKey)FieldData.ReadValue(v.name, _KeyDataType, _KeyType);
-                TValue value = (TValue)FieldData.ReadValue(v.value, _ValueDataType, _ValueType);
-                if (ContainsKey(key))
-                {
-                    Debug.LogError($"PersistentDictionary: Contains key {key}");
-                    Remove(key);
-                }
-                Add(key, value);
-            }
-        }
-
-        public void Save(ConfigNode node)
-        {
-            foreach (var kvp in this)
-            {
-
-                string key = FieldData.WriteValue(kvp.Key, _KeyDataType);
-                string value = FieldData.WriteValue(kvp.Value, _ValueDataType);
-                node.AddValue(key, value);
-            }
-        }
+        public PersistentDictionaryValueTypes() { _handler = new IDictionaryPersistenceValueTypes<TKey, TValue>(this); }
 
         public void Clone(PersistentDictionaryValueTypes<TKey, TValue> source)
         {
--- Source/ROUtils/DataTypes/PersistentSortedList.cs
+++ Source/ROUtils/DataTypes/PersistentSortedList.cs
@@ -4,64 +4,16 @@ using KSPCommunityFixes.Modding;
 
 namespace ROUtils.DataTypes
 {
-    public abstract class PersistentSortedList<TKey, TValue> : SortedList<TKey, TValue>, IConfigNode where TValue : IConfigNode
+    public abstract class PersistentSortedList<TKey, TValue> : SortedList<TKey, TValue>, IConfigNode
     {
-        private static readonly Type _Type = typeof(TValue);
-        private static readonly string _TypeName = typeof(TValue).Name;
-        private static readonly Dictionary<string, Type> _TypeCache = new Dictionary<string, Type>();
-
-        protected abstract TKey ParseKey(string key);
-
-        protected abstract string WriteKey(TKey key);
-
         public void Load(ConfigNode node)
         {
-            Clear();
-            ConfigNode keyNode = node.nodes[0];
-            ConfigNode valueNode = node.nodes[1];
-            int version = 1;
-            node.TryGetValue("version", ref version);
-
-            for (int i = 0; i < keyNode.values.Count; ++i)
-            {
-                TKey key = ParseKey(keyNode.values[i].value);
-
-                var n = valueNode.nodes[i];
-                TValue value;
-                if (version == 1 || n.name == "VALUE" || n.name == _TypeName)
-                {
-                    value = Activator.CreateInstance<TValue>();
-                }
-                else
-                {
-                    if (!_TypeCache.TryGetValue(n.name, out var type))
-                        type = HarmonyLib.AccessTools.TypeByName(n.name);
-                    if (type == null || !_Type.IsAssignableFrom(type))
-                        type = _Type;
-                    else
-                        _TypeCache[n.name] = type;
-
-                    value = (TValue)Activator.CreateInstance(type);
-                }
-                value.Load(n);
-                Add(key, value);
-            }
+            ConfigNode.LoadObjectFromConfig(this, node);
         }
 
         public void Save(ConfigNode node)
         {
-            node.AddValue("version", 2);
-            ConfigNode keyNode = node.AddNode("Keys");
-            ConfigNode valueNode = node.AddNode("Values");
-
-            foreach (var kvp in this)
-            {
-                keyNode.AddValue("key", WriteKey(kvp.Key));
-                var type = kvp.Value.GetType();
-                ConfigNode n = new ConfigNode(type == _Type ? _TypeName : type.FullName);
-                kvp.Value.Save(n);
-                valueNode.AddNode(n);
-            }
+            ConfigNode.CreateConfigFromObject(this, node);
         }
     }
 
@@ -72,18 +24,20 @@ namespace ROUtils.DataTypes
     /// <typeparam name="TValue"></typeparam>
     public class PersistentSortedListValueTypeKey<TKey, TValue> : PersistentSortedList<TKey, TValue> where TValue : IConfigNode
     {
-        private static readonly Type _KeyType = typeof(TKey);
-        private static readonly DataType _KeyDataType = FieldData.ValueDataType(_KeyType);
+        [Persistent]
+        protected IDictionaryPersistenceValueTypeKey<TKey, TValue> _handler;
 
-        protected override TKey ParseKey(string value)
-        {
-            return (TKey)FieldData.ReadValue(value, _KeyDataType, _KeyType);
-        }
+        public PersistentSortedListValueTypeKey() { _handler = new IDictionaryPersistenceValueTypeKey<TKey, TValue>(this); }
+    }
 
-        protected override string WriteKey(TKey key)
-        {
-            return FieldData.WriteValue(key, _KeyDataType);
-        }
+    public class PersistentSortedListNodeValueKeyed<TKey, TValue> : PersistentSortedList<TKey, TValue>, IConfigNode where TValue : IConfigNode
+    {
+        [Persistent]
+        protected IDictionaryPersistenceNodeValueKeyed<TKey, TValue> _handler;
+
+        public PersistentSortedListNodeValueKeyed() { _handler = new IDictionaryPersistenceNodeValueKeyed<TKey, TValue>(this); }
+
+        public PersistentSortedListNodeValueKeyed(string keyName) { _handler = new IDictionaryPersistenceNodeValueKeyed<TKey, TValue>(this, keyName); }
     }
 
     /// <summary>
@@ -92,58 +46,11 @@ namespace ROUtils.DataTypes
     /// </summary>
     /// <typeparam name="TKey"></typeparam>
     /// <typeparam name="TValue"></typeparam>
-    public class PersistentSortedListValueTypes<TKey, TValue> : SortedList<TKey, TValue>, IConfigNode
+    public class PersistentSortedListValueTypes<TKey, TValue> : PersistentSortedList<TKey, TValue>, IConfigNode
     {
-        private static Type _KeyType = typeof(TKey);
-        private static readonly DataType _KeyDataType = FieldData.ValueDataType(_KeyType);
-        private static Type _ValueType = typeof(TValue);
-        private static readonly DataType _ValueDataType = FieldData.ValueDataType(_ValueType);
+        [Persistent]
+        protected IDictionaryPersistenceValueTypes<TKey, TValue> _handler;
 
-        public void Load(ConfigNode node)
-        {
-            Clear();
-            foreach (ConfigNode.Value v in node.values)
-            {
-                TKey key = (TKey)FieldData.ReadValue(v.name, _KeyDataType, _KeyType);
-                TValue value = (TValue)FieldData.ReadValue(v.value, _ValueDataType, _ValueType);
-                Add(key, value);
-            }
-        }
-
-        public void Save(ConfigNode node)
-        {
-            foreach (var kvp in this)
-            {
-                string key = FieldData.WriteValue(kvp.Key, _KeyDataType);
-                string value = FieldData.WriteValue(kvp.Value, _ValueDataType);
-                node.AddValue(key, value);
-            }
-        }
-
-        public void Clone(PersistentSortedListValueTypes<TKey, TValue> source)
-        {
-            Clear();
-            foreach (var kvp in source)
-                Add(kvp.Key, kvp.Value);
-        }
-
-        public PersistentSortedListValueTypes<TKey, TValue> Clone()
-        {
-            var dict = new PersistentSortedListValueTypes<TKey, TValue>();
-            foreach (var kvp in this)
-                dict.Add(kvp.Key, kvp.Value);
-
-            return dict;
-        }
-
-        public static bool AreEqual(Dictionary<TKey, TValue> d1, Dictionary<TKey, TValue> d2)
-        {
-            if (d1.Count != d2.Count) return false;
-            foreach (TKey key in d1.Keys)
-                if (!d2.TryGetValue(key, out TValue val) || !val.Equals(d1[key]))
-                    return false;
-
-            return true;
-        }
+        public PersistentSortedListValueTypes() { _handler = new IDictionaryPersistenceValueTypes<TKey, TValue>(this); }
     }
 }
