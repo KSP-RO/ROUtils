cecf94e0db439de59b212a6f6bc296f9e9327ec6
NathanKell <nathankell@gmail.com>
2023-11-22 17:19:16 -0800

    Now that most of the code is moved out, remove the separate CollectionDictionary file

--- Source/ROUtils/DataTypes/PersistentDictionary.cs
+++ Source/ROUtils/DataTypes/PersistentDictionary.cs
@@ -101,4 +101,157 @@ namespace ROUtils.DataTypes
             return true;
         }
     }
+
+    // Due to lack of multiple inheritance, this is a bunch of copypasta.
+    // Happily all each class needs is the add and remove methods and the AllValues collection/constructor
+
+    public class PersistentDictionaryBothObjects<TKey, TValue, TCollection> : PersistentDictionaryBothObjects<TKey, TCollection>, IConfigNode where TKey : IConfigNode where TCollection : ICollection<TValue>, IConfigNode, new()
+    {
+        private CollectionDictionaryAllValues<TKey, TValue, TCollection> _allValues;
+        public CollectionDictionaryAllValues<TKey, TValue, TCollection> AllValues => _allValues;
+
+        public PersistentDictionaryBothObjects() : base() { _allValues = new CollectionDictionaryAllValues<TKey, TValue, TCollection>(this); }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+            {
+                coll = new TCollection();
+                Add(key, coll);
+            }
+
+            coll.Add(value);
+        }
+
+        public bool Remove(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+                return false;
+
+            if (!coll.Remove(value))
+                return false;
+
+            if (coll.Count == 0)
+                Remove(key);
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// This does not have a struct constraint because string is not a valuetype but can be handled by ConfigNode's parser
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class PersistentDictionaryNodeValueKeyed<TKey, TValue, TCollection> : PersistentDictionaryNodeValueKeyed<TKey, TCollection>, IConfigNode where TCollection : ICollection<TValue>, IConfigNode, new()
+    {
+        private CollectionDictionaryAllValues<TKey, TValue, TCollection> _allValues;
+        public CollectionDictionaryAllValues<TKey, TValue, TCollection> AllValues => _allValues;
+
+        public PersistentDictionaryNodeValueKeyed() : base() { _allValues = new CollectionDictionaryAllValues<TKey, TValue, TCollection>(this); }
+
+        public PersistentDictionaryNodeValueKeyed(string keyName) : base(keyName) { _allValues = new CollectionDictionaryAllValues<TKey, TValue, TCollection>(this); }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+            {
+                coll = new TCollection();
+                Add(key, coll);
+            }
+
+            coll.Add(value);
+        }
+
+        public bool Remove(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+                return false;
+
+            if (!coll.Remove(value))
+                return false;
+
+            if (coll.Count == 0)
+                Remove(key);
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// This does not have a struct constraint because string is not a valuetype but can be handled by ConfigNode's parser
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class PersistentDictionaryValueTypeKey<TKey, TValue, TCollection> : PersistentDictionaryValueTypeKey<TKey, TCollection> where TCollection : ICollection<TValue>, IConfigNode, new()
+    {
+        private CollectionDictionaryAllValues<TKey, TValue, TCollection> _allValues;
+        public CollectionDictionaryAllValues<TKey, TValue, TCollection> AllValues => _allValues;
+
+        public PersistentDictionaryValueTypeKey() : base() { _allValues = new CollectionDictionaryAllValues<TKey, TValue, TCollection>(this); }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+            {
+                coll = new TCollection();
+                Add(key, coll);
+            }
+
+            coll.Add(value);
+        }
+
+        public bool Remove(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+                return false;
+
+            if (!coll.Remove(value))
+                return false;
+
+            if (coll.Count == 0)
+                Remove(key);
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// NOTE: This does not have constraints because string is supported
+    /// but string is not a valuetype
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class PersistentDictionaryValueTypes<TKey, TValue, TCollection> : PersistentDictionaryValueTypeKey<TKey, TCollection>, IConfigNode where TCollection : ICollection<TValue>, IConfigNode, new()
+    {
+        private CollectionDictionaryAllValues<TKey, TValue, TCollection> _allValues;
+        public CollectionDictionaryAllValues<TKey, TValue, TCollection> AllValues => _allValues;
+
+        public PersistentDictionaryValueTypes() : base() { _allValues = new CollectionDictionaryAllValues<TKey, TValue, TCollection>(this); }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+            {
+                coll = new TCollection();
+                Add(key, coll);
+            }
+
+            coll.Add(value);
+        }
+
+        public bool Remove(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+                return false;
+
+            if (!coll.Remove(value))
+                return false;
+
+            if (coll.Count == 0)
+                Remove(key);
+
+            return true;
+        }
+    }
 }
--- Source/ROUtils/DataTypes/PersistentSortedList.cs
+++ Source/ROUtils/DataTypes/PersistentSortedList.cs
@@ -53,4 +53,124 @@ namespace ROUtils.DataTypes
 
         public PersistentSortedListValueTypes() { _handler = new IDictionaryPersistenceValueTypes<TKey, TValue>(this); }
     }
+
+    // Due to lack of multiple inheritance, this is a bunch of copypasta.
+    // Happily all each class needs is the add and remove methods and the AllValues collection/constructor
+
+    /// <summary>
+    /// This does not have a struct constraint because string is not a valuetype but can be handled by ConfigNode's parser
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class PersistentSortedListNodeValueKeyed<TKey, TValue, TCollection> : PersistentSortedListNodeValueKeyed<TKey, TCollection>, IConfigNode where TCollection : ICollection<TValue>, IConfigNode, new()
+    {
+        private CollectionDictionaryAllValues<TKey, TValue, TCollection> _allValues;
+        public CollectionDictionaryAllValues<TKey, TValue, TCollection> AllValues => _allValues;
+
+        public PersistentSortedListNodeValueKeyed() : base() { _allValues = new CollectionDictionaryAllValues<TKey, TValue, TCollection>(this); }
+
+        public PersistentSortedListNodeValueKeyed(string keyName) : base(keyName) { _allValues = new CollectionDictionaryAllValues<TKey, TValue, TCollection>(this); }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+            {
+                coll = new TCollection();
+                Add(key, coll);
+            }
+
+            coll.Add(value);
+        }
+
+        public bool Remove(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+                return false;
+
+            if (!coll.Remove(value))
+                return false;
+
+            if (coll.Count == 0)
+                Remove(key);
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// This does not have a struct constraint because string is not a valuetype but can be handled by ConfigNode's parser
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class PersistentSortedListValueTypeKey<TKey, TValue, TCollection> : PersistentSortedListValueTypeKey<TKey, TCollection> where TCollection : ICollection<TValue>, IConfigNode, new()
+    {
+        private CollectionDictionaryAllValues<TKey, TValue, TCollection> _allValues;
+        public CollectionDictionaryAllValues<TKey, TValue, TCollection> AllValues => _allValues;
+
+        public PersistentSortedListValueTypeKey() : base() { _allValues = new CollectionDictionaryAllValues<TKey, TValue, TCollection>(this); }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+            {
+                coll = new TCollection();
+                Add(key, coll);
+            }
+
+            coll.Add(value);
+        }
+
+        public bool Remove(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+                return false;
+
+            if (!coll.Remove(value))
+                return false;
+
+            if (coll.Count == 0)
+                Remove(key);
+
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// NOTE: This does not have constraints because string is supported
+    /// but string is not a valuetype
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public class PersistentSortedListValueTypes<TKey, TValue, TCollection> : PersistentSortedListValueTypeKey<TKey, TCollection>, IConfigNode where TCollection : ICollection<TValue>, IConfigNode, new()
+    {
+        private CollectionDictionaryAllValues<TKey, TValue, TCollection> _allValues;
+        public CollectionDictionaryAllValues<TKey, TValue, TCollection> AllValues => _allValues;
+
+        public PersistentSortedListValueTypes() : base() { _allValues = new CollectionDictionaryAllValues<TKey, TValue, TCollection>(this); }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+            {
+                coll = new TCollection();
+                Add(key, coll);
+            }
+
+            coll.Add(value);
+        }
+
+        public bool Remove(TKey key, TValue value)
+        {
+            if (!TryGetValue(key, out var coll))
+                return false;
+
+            if (!coll.Remove(value))
+                return false;
+
+            if (coll.Count == 0)
+                Remove(key);
+
+            return true;
+        }
+    }
 }
