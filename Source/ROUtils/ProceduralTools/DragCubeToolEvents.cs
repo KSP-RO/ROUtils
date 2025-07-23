c9ad1029ed3ff2a070efdaf1478f9a42dc4ee12d
siimav <1120038+siimav@users.noreply.github.com>
2025-07-23 23:49:19 +0300

    Add common DragCubeTool, now with caching (#1)

---
+++ Source/ROUtils/ProceduralTools/DragCubeToolEvents.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace ROUtils
+{
+    internal class DragCubeToolEvents : HostedSingleton
+    {
+        public DragCubeToolEvents(SingletonHost host) : base(host)
+        {
+        }
+
+        public override void Awake()
+        {
+            SceneManager.sceneLoaded += SceneLoaded;
+        }
+
+        private void SceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            DragCubeTool.ClearStaticState();
+        }
+    }
+}
